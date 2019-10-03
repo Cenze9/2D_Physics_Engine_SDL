ef9ed03d0bd2031cb6bbb34f2e9f23e6d914ea5d
Henri <henri.lehtimaki@hotmail.com>
2019-10-03 11:33:09 +0300

    Safekeep commit

---
+++ 2D_Physics/GameObject.h
@@ -0,0 +1,22 @@
+#ifndef GameObject_h
+#define GameObject_h
+
+#include "SDL.h"
+
+class GameObject
+{
+public:
+    GameObject();
+    ~GameObject();
+
+
+    void update();
+private:
+
+
+    
+    
+
+};
+
+#endif
